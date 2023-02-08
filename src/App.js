ee98a76c3008d51532ee8dde043810a1483d2548
setyawannn <prayogasetyawan.adi@gmail.com>
2023-02-08 07:25:57 +0700

    set-up ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-red-600'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen flex justify-center items-center'>
+      <h1 className="text-4xl font-bold text-blue-600">Hello world . . .</h1>
     </div>
   );
 }
